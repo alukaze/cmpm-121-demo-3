f7a3d96851f587c9084d182523523cae3912b1b9
Alan Lu <alu36@ucsc.edu>
2024-11-07 17:10:00 -0800

    L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.createElement("button");
+    button.textContent = "Click Here!";
+    document.body.appendChild(button);
+  
+    button.addEventListener("click", () => {
+      alert("Welcome to Demo 3");
+    });
+  });
+  
